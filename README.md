e252b6fcf32c5f0b3e899d991ee7d9772ecc4678
Joey Douek <joeyblue27@gmail.com>
2023-02-10 11:31:31 -0500

    Html Bootstrap Display

---
+++ README.md
@@ -0,0 +1,9 @@
+## Skycast Weather Dashboard
+
+* The weather dashboard app shows a city with a 5 day forecast. 
+* Displays forecast with temparatures, humidity and winds.
+* List previous city searches that can be revisted.
+* Current city shows weather icons with future conditions.
+* Easy navigation and user friendly display. 
+
+
---
+++ js/script.js
@@ -0,0 +1,14 @@
+ // create weather variables  
+var apiKey = '0b61484c8ea3e10a2bcde29856092795';
+var weatherUrl= 'https://api.openweathermap.org';
+var showHistory= [];
+var showTemperature = document.querySelector('#temperature');
+var showHumidty = document.querySelector('#humidity');
+var showWinds = document.querySelector('#wind-mph');
+var inputSearch = document.querySelector('#input-search');
+var inputBtn = document.querySelector('#input-button');
+var removeBtn = document.querySelector('#remove');
+var showCity = document.querySelector('#show-city');
+
+
+
